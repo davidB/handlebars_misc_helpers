a7e04b2c04836421a1cb33626774c60783920bc1
David Bernard <david.bernard.31@gmail.com>
2019-09-10 23:14:06 +0200

    :minus: replace `reqwest` by `attohttpc` to dicrease the number of dependencies

--- src/http_helpers.rs
+++ src/http_helpers.rs
@@ -1,33 +1,51 @@
 use handlebars::HelperDef;
+use handlebars::RenderError;
 use handlebars::{handlebars_helper, Handlebars};
-use reqwest;
+use attohttpc;
 
-fn http_get_fct<T: AsRef<str>>(url: T) -> String {
-    match reqwest::get(url.as_ref()).and_then(|mut r| r.text()) {
-        Ok(s) => s,
-        Err(e) => {
-            //TODO better error handler
-            //use slog::warn;
-            //warn!(ctx.logger, "helper: http_get"; "url" => format!("{:?}", url), "err" => format!("{:?}", e))
-            eprintln!(
-                "helper: http_get failed for url '{:?}' with error '{:?}'",
-                url.as_ref(),
-                e
-            );
-            "".to_owned()
-        }
-    }
+fn http_get_fct<T: AsRef<str>>(url: T) -> Result<String, attohttpc::Error> {
+    attohttpc::get(url.as_ref()).send()?.text()
 }
 
 pub fn register(handlebars: &mut Handlebars) -> Vec<Box<dyn HelperDef + 'static>> {
     vec![{
-            handlebars_helper!(http_get: |v: str| http_get_fct(&v));
+            handlebars_helper!(http_get: |v: str| http_get_fct(&v).map_err(RenderError::with)?);
             handlebars.register_helper("http_get", Box::new(http_get))
         },{
-            handlebars_helper!(gitignore_io: |v: str| http_get_fct(format!("https://www.gitignore.io/api/{}", v)));
+            handlebars_helper!(gitignore_io: |v: str| http_get_fct(format!("https://www.gitignore.io/api/{}", v)).map_err(RenderError::with)?);
             handlebars.register_helper("gitignore_io", Box::new(gitignore_io))
-         }]
+        }]
         .into_iter()
         .flatten()
         .collect()
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     // use crate::tests::assert_renders;
+//     use spectral::prelude::*;
+//     use std::error::Error;
+
+//     #[test]
+//     fn try_http_get_fct() -> Result<(), Box<dyn Error>> {
+//         assert_that!(http_get_fct("https://www.gitignore.io/api/text")?).is_equal_to(r#"
+// # Created by https://www.gitignore.io/api/text
+// # Edit at https://www.gitignore.io/?templates=text
+
+// ### Text ###
+// *.doc
+// *.docx
+// *.log
+// *.msg
+// *.pages
+// *.rtf
+// *.txt
+// *.wpd
+// *.wps
+
+// # End of https://www.gitignore.io/api/text
+// "#.to_string());
+//         Ok(())
+//     }
+// }
